b1653d5fbb287fbf0f0545d4f97f9be689a62b80
Alvaro <alvaroariasrivero92@gmail.com>
2021-10-07 17:02:26 +0200

    grafica de barras de Una Nueva Esperanza

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// fetch('https://swapi.dev/api/films/')
-//     .then(res => res.json())
-//     .then(json => console.log(json))
+fetch('https://swapi.dev/api/films/')
+    .then(res => res.json())
+    .then(json => console.log(json))
 
 async function nombre() {
     try { 
@@ -13,15 +13,15 @@ async function nombre() {
             title: pelis,
             fecha: fechas
         } 
-        console.log(objEpi)
+        // console.log(objEpi)
         for (let i = 0; i < episodio.length; i++) {
             let valorEpi = i + 1
             // console.log(valorEpi)
             let urlEpi = await fetch(`https://swapi.dev/api/films/${valorEpi}`)
-            let response = await urlEpi.json()
+            let res = await urlEpi.json()
             // console.log(urlEpi)
-            pelis.push(response.title)
-            fechas.push(parseFloat(response.release_date.slice(0, 4)))
+            pelis.push(res.title)
+            fechas.push(parseFloat(res.release_date.slice(0, 4)))
         } 
             return objEpi
         // return impreso
@@ -38,10 +38,53 @@ nombre().then(res => {
     var options = {
         axisY: {
             onlyInteger: true
-
         }
     };
-    new Chartist.Line('.ct-chart', data, options);
+    new Chartist.Line('#lineas', data, options);
 
 }) //res=argumento que paso para devolverme el return de la función
 
+async function personaje() {
+    try {
+        let response = await fetch('https://swapi.dev/api/films/')
+        let data = await response.json()
+        let episodio = await data.results[0].characters
+        // console.log(episodio)
+        let pelis = []
+        let personajes = []
+        let objChar = {
+            personaje: personajes,
+            apariciones: pelis
+        }
+        for(let i = 0; i < episodio.length; i++) {
+            valorPer = i + 1
+            // console.log(valorPer)
+            let urlChar = await fetch(`https://swapi.dev/api/people/${valorPer}`)
+            let res = await urlChar.json()
+            // console.log(res.films)
+            // console.log(urlChar)
+            if(!res.detail) {
+                personajes.push(res.name)
+                pelis.push(res.films.length)
+
+            }
+        }console.log(objChar)
+        return objChar
+        
+    }catch (error) {
+    console.log(`ERROR: ${error.stack}`);
+    }
+}
+
+personaje().then(res => {
+    var data = {
+        labels: res.personaje,
+        series: [res.apariciones]
+    }
+    var options = {
+        axisY: {
+            onlyInteger: true
+        }
+    };
+    new Chartist.Bar('#barras', data, options)
+})
