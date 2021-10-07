e853eaf752cbe1c5d50e52d3a0d5e7db5a2a787b
Alvaro <alvaroariasrivero92@gmail.com>
2021-10-07 14:23:36 +0200

    gráfico creado

--- script.js
+++ script.js
@@ -7,11 +7,41 @@ async function nombre() {
         let response = await fetch('https://swapi.dev/api/films/')
         let data = await response.json()
         let episodio = await data.results
-        return episodio
-
+        let pelis = []
+        let fechas = []
+        let objEpi = {
+            title: pelis,
+            fecha: fechas
+        } 
+        console.log(objEpi)
+        for (let i = 0; i < episodio.length; i++) {
+            let valorEpi = i + 1
+            // console.log(valorEpi)
+            let urlEpi = await fetch(`https://swapi.dev/api/films/${valorEpi}`)
+            let response = await urlEpi.json()
+            // console.log(urlEpi)
+            pelis.push(response.title)
+            fechas.push(parseFloat(response.release_date.slice(0, 4)))
+        } 
+            return objEpi
+        // return impreso
     } catch (error) {
         console.log(`ERROR: ${error.stack}`);
     }
 }
 
-nombre().then(res => console.log(res)) //res=argumento que paso para devolverme el return de la función
+nombre().then(res => {
+    var data = {
+        labels: res.title,
+        series: [res.fecha]
+    };
+    var options = {
+        axisY: {
+            onlyInteger: true
+
+        }
+    };
+    new Chartist.Line('.ct-chart', data, options);
+
+}) //res=argumento que paso para devolverme el return de la función
+
